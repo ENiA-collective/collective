34e329946d2ada6b4b63580e5fa76942960a6c60
epifaniagb <epifaniaegb@gmail.com>
2024-05-14 15:40:58 -0400

    listings homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchListings } from '../adapters/listing-adapter';
+import fetchListings from '../adapters/listing-adapter';
 
 const HomePage = () => {
   const [listings, setListings] = useState([]);
