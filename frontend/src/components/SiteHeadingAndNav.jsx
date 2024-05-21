98b5c82e62bd780d4d1023c78b1ae29a501120df
alexaleoto <alexaricard@hotmail.com>
2024-05-21 17:55:30 -0400

    navbar

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -5,28 +5,49 @@ import CurrentUserContext from "../contexts/CurrentUserContext.jsx";
 export default function SiteHeadingAndNav() {
   const { currentUser } = useContext(CurrentUserContext);
 
-  return <header>
-    <a id='logo' href='/'>Collective</a>
-    <nav>
-      <ul>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-
-        {
-          currentUser
-            ? <>
-              <li><NavLink to='/post'>Post</NavLink></li>
-              {/* <li><NavLink to='/users' end={true}>Users</NavLink></li> we don't have a frontend page that shows all users*/}
-              <li><NavLink to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink></li>
-            </>
-            : <>
-              <li><NavLink to='/login'>Login</NavLink></li>
-              <li><NavLink to='/signup'>Sign Up</NavLink></li>
-              {/* removed My Profile link because it's only visible to users who aren't logged in. Aka users with no profile*/}
-            </>
-        }
-      </ul>
-    </nav>
-  </header>;
+  return (
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+        <div className="flex items-center justify-between">
+          <a className="flex-none text-xl font-semibold dark:text-white" href="/">Collective</a>
+          <div className="sm:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10"
+              data-hs-collapse="#navbar-with-collapse"
+              aria-controls="navbar-with-collapse"
+              aria-label="Toggle navigation"
+            >
+              <svg className="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg className="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div id="navbar-with-collapse" className="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
+          <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+            <NavLink className="font-medium text-blue-500" to="/">Home</NavLink>
+            <NavLink className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" to="/about">About</NavLink>
+            {currentUser ? (
+              <>
+                <NavLink className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" to="/post">Post</NavLink>
+                <NavLink className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" to="/login">Login</NavLink>
+                <NavLink className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" to="/signup">Sign Up</NavLink>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
-
