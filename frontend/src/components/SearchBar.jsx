4ec7a1e46da433e986c7cd411137b68b9c3e2944
alexaleoto <alexaricard@hotmail.com>
2024-05-22 10:21:47 -0400

    style changes

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
-const SearchBar = ({handleSearch}) => {
-  const [query, setQuery] = useState('')
+const SearchBar = ({ handleSearch }) => {
+  const [query, setQuery] = useState('');
 
-  return <>
-    <label class="content-center" htmlFor="search-bar">Search: </label>
-    <input
-      id='search-bar'
-      onChange={(e) => setQuery(e.target.value)}
-      value={query}
-      required
-      maxLength={250}
-    />
-    <button onClick={() => handleSearch(query)}  className="bg-text text-white rounded-full font-semibold text-lg px-6 py-3 cursor-pointer transition-all duration-300 ease-in-out border border-black shadow-none hover:-translate-y-1 hover:-translate-x-0.5 hover:shadow-[2px_5px_0_0_black] active:translate-y-0.5 active:translate-x-0.25 active:shadow-none">Search</button>
-  </>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <label htmlFor="search-bar">Search: </label>
+      <input
+        id="search-bar"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        required
+        maxLength={250}
+      />
+      <button
+        onClick={() => handleSearch(query)}
+        className="bg-text text-white rounded-full font-semibold text-lg px-6 py-3 cursor-pointer transition-all duration-300 ease-in-out border border-black shadow-none hover:-translate-y-1 hover:-translate-x-0.5 hover:shadow-[2px_5px_0_0_black] active:translate-y-0.5 active:translate-x-0.25 active:shadow-none"
+      >
+        Search
+      </button>
+    </div>
+  );
 };
 
-export default SearchBar
+export default SearchBar;
